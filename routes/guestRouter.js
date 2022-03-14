fcc749bc3e2d1e2a792288c1f2d55c51205bb6a6
samuel-yeebiyo <samoyemaneyy@gmail.com>
2022-03-14 03:26:21 +0000

    Added endpoint for a guest cart to be transferred to the user when logging in

--- routes/guestRouter.js
+++ routes/guestRouter.js
@@ -4,13 +4,18 @@ const Router = express.Router()
 const User = require('../models/User')
 const GuestOrder = require('../models/guestOrder')
 
+
+//route to update guest cart
 Router.post('/:id/cart/update', async (req,res)=>{
 
     console.log("Ready to update cart for guest")
     
+    //find if a cart exists for the guest that is not fulfilled
     const guestOrder = await GuestOrder.findOne({guestId: req.params.id, fulfilled:false})
 
     if(guestOrder == null){
+        
+        //create guest order if none
         let orderData = new GuestOrder({
             guestId: req.params.id,
             ...req.body
@@ -27,14 +32,15 @@ Router.post('/:id/cart/update', async (req,res)=>{
         }
     }else{
        
-       guestOrder.items = [...req.body.items],
-       guestOrder.subtotal = req.body.subtotal
-       try{
-            await guestOrder.save().then(doc =>{
-                res.send("success")
-            })
-        }catch(e){
-            console.log("Problem with saving")
+        //update an existing cart
+        guestOrder.items = [...req.body.items],
+        guestOrder.subtotal = req.body.subtotal
+        try{
+                await guestOrder.save().then(doc =>{
+                    res.send("success")
+                })
+            }catch(e){
+                console.log("Problem with saving")
         }
     }  
 })
@@ -42,9 +48,12 @@ Router.post('/:id/cart/update', async (req,res)=>{
 Router.get('/:id/cart', async(req, res) =>{
     console.log("Getting cart")
 
+    //find the cart for the guest
     const guestOrder = await GuestOrder.findOne({guestId: req.params.id, fulfilled:false})
 
     if(guestOrder != null){
+
+        //send item and subtotal - relevant stuff
         res.send({
             items:guestOrder.items,
             subtotal:guestOrder.subtotal
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,7 +2,9 @@ const express = require('express')
 const Router = express.Router()
 
 const User = require('../models/User')
+
 const UserOrder = require('../models/userOrder')
+const GuestOrder = require('../models/guestOrder')
 
 async function authUser (req, res, next){
     let user = await User.findOne({_id: req.params.id})
@@ -27,8 +29,7 @@ Router.post('/:id/cart/update', authUser , async (req,res)=>{
 
         try{
             await orderData.save().then(doc =>{
-                console.log(doc)
-                res.status(200)
+                res.send("success")
             })
         }catch(e){
             console.log(e)
@@ -39,8 +40,8 @@ Router.post('/:id/cart/update', authUser , async (req,res)=>{
        userOrder.subtotal = req.body.subtotal
        try{
             await userOrder.save().then(doc =>{
-                console.log(doc)
-                res.status(200)
+                res.send("success")
+
             })
         }catch(e){
             console.log("Problem with saving")
@@ -48,8 +49,70 @@ Router.post('/:id/cart/update', authUser , async (req,res)=>{
     }
 })
 
+Router.post('/:id/transfer/:guest', authUser, async(req, res)=>{
+    console.log("Transferring cart to user")
 
-Router.get('/:id/cart', async(req, res) =>{
+    //check if user already has an unfulfilled order
+    const userOrder = await UserOrder.findOne({userId:req.params.id, fulfilled:false})
+    //make sure a cart with the guestid exists
+    const guestOrder = await GuestOrder.findOne({guestId:req.params.guest, fulfilled:false})
+
+    if(userOrder == null && guestOrder != null){
+        console.log("Creating a new user order")
+
+        //creating new cart with previous guest item
+        let orderData = new UserOrder({
+            userId: req.params.id,
+            items: [...guestOrder.items],
+            subtotal:guestOrder.subtotal
+        })
+
+        try{
+            await orderData.save().then(doc =>{
+                res.send("success")
+            })
+
+            //deleting guest order
+            await guestOrder.delete()
+        }catch(e){
+            console.log(e)
+        }
+    }else{
+        console.log("Updating existing user order")
+
+        //consolidating items, quantities, and total price of existing and guest cart
+        guestOrder.items.map((guestItem)=>{
+            let exist = false;
+            userOrder.items.map((userItem)=>{
+                if(guestItem.itemId == userItem.itemId){
+                    userItem.quantity+=guestItem.quantity
+                    exist=true
+                    return
+                }
+            })
+            if(!exist){
+                userOrder.items.push(guestItem)
+            }
+        })
+        userOrder.subtotal += guestOrder.subtotal
+
+        try{
+            await userOrder.save().then(doc =>{
+                res.send("success")
+            })
+
+            //deleting guest orders
+            await guestOrder.delete()
+        }catch(e){
+            console.log(e)
+        }
+        
+    }
+
+
+})
+
+Router.get('/:id/cart', authUser, async(req, res) =>{
     console.log("Getting cart")
 
     const userOrder = await UserOrder.findOne({userId: req.params.id, fulfilled:false})
@@ -60,7 +123,6 @@ Router.get('/:id/cart', async(req, res) =>{
             subtotal:userOrder.subtotal
         })
     }
-
 
 })
 
